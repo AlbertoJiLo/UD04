cd001f5b4cab11f2d8f6515001dd3cde706bffe5
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-09-30 16:57:55 +0200

    Eclipse prueba 2

---
+++ UD04/ex1.java
@@ -0,0 +1,26 @@
+package UD04;
+
+public class ex1 {
+	
+	public static void main (String[] args) {
+		int num1=20;
+		int num2=10;
+		int suma;
+		int resta;
+		int multi;
+		int divi;
+		int modulo;
+		
+		suma=num1+num2;
+		System.out.println(suma);
+		resta=num1-num2;
+		System.out.println(resta);
+		multi=num1*num2;
+		System.out.println(multi);
+		divi=num1/num2;
+		System.out.println(divi);
+		modulo=num1%num2;
+		System.out.println(modulo);
+	}
+
+}
