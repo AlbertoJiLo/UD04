9c81c4fd3366aef2b4a5e4903e461083fcfa509e
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-10-05 19:38:45 +0200

    error 1

--- UD04/ex5.java
+++ UD04/ex5.java
@@ -6,19 +6,22 @@ public class ex5 {
 		int B=5;
 		int C=7;
 		int D=11;
-		
+		int A2=2;
+		int B2=5;
+		int C2=7;
+		int D2=11;
 		System.out.println("A = "+A+" B= " +B+" C= "+C+" D = "+D);
-		B=C;
+		B=C2;
 		System.out.println("B toma el valor de C");
 		System.out.println("A = "+A+" B= " +B+" C= "+C+" D = "+D);
-		C=A;
+		C=A2;
 		System.out.println("C toma el valor de A");
 		System.out.println("A = "+A+" B= " +B+" C= "+C+" D = "+D);
-		A=D;
+		A=D2;
 		System.out.println("A toma el valor de D");
 		System.out.println("A = "+A+" B= " +B+" C= "+C+" D = "+D);
 		System.out.println("D toma el valor de B");
-		D=B;
+		D=B2;
 		System.out.println("A = "+A+" B= " +B+" C= "+C+" D = "+D);
 		
 		
